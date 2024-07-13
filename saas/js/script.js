596bac36db10cf614900b4353da52a599cd9b005
yulch <159718838+yulchmalan@users.noreply.github.com>
2024-07-13 06:04:10 +0300

    Add files via upload

--- saas/js/script.js
+++ saas/js/script.js
@@ -1,18 +1,13 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    type: 'bullets',
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    prevEl: '.prev',
+    nextEl: '.next',
+  }
+});
+
